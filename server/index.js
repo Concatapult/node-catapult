6bdfdb02d541075ffc1893ae41def7dabee24a8c
Gilbert <gilbertbgarza@gmail.com>
2015-08-19 10:48:12 -0500

    Support pushstate by default

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var browserify = require('browserify-middleware')
 var express = require('express')
 var app = express()
+var Path = require('path')
 
 // Provide a browserified file at a specified path
 app.get('/js/app-bundle.js',
@@ -8,6 +9,14 @@ app.get('/js/app-bundle.js',
 
 // Non-js static files
 app.use(express.static('client/public'))
+
+//
+// Support browser history pushstate.
+// NOTE: Make sure this route is always last.
+//
+app.get('/*', function(req, res){
+  res.sendFile( Path.resolve(__dirname, '../client/public/index.html') )
+})
 
 var port = process.env.PORT || 4000
 app.listen(port)
